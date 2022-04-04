53705c8cb2da85cd6643703883fe2cbdbb77441f
minC0819 <85930979+minC0819@users.noreply.github.com>
2022-04-04 20:26:37 +0800

    pull commit

--- src/main/java/Git/GitTest.java
+++ src/main/java/Git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("Hello!");
         System.out.println("Hello!");
         System.out.println("57146566@qq.com");
-        System.out.println("57146566@qq.com1");
+        System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
